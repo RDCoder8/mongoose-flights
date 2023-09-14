c9908bb060e16916dd9e988ee60dd192649d62b7
RDCoder8 <mojinr@gmail.com>
2023-09-14 09:14:49 -0400

    added destination schema and airport select option

--- models/Flight.js
+++ models/Flight.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose')
 
+const destinationSchema = new mongoose.Schema({
+    airport: { type: String, required: true, enum: ['AUS', 'DAL', 'LAX', 'SAN', 'SEA']},
+    arrival: Date
+})
+
 const flightSchema = new mongoose.Schema({
     airline: { type: String, required: true, enum: ['American', 'Southwest', 'United'] },
     flightNo: {type: Number,  required: true, min: 10, max: 9999},
-    departs: {type: Date, default: Date.now()}
+    departs: {type: Date},
+    airport: { type: String, required: true, enum: ['AUS', 'DAL', 'LAX', 'SAN', 'SEA'], default: 'SAN'},
+    destinations: [destinationSchema]
 }, {
     timestamps: true
 })
--- views/New.jsx
+++ views/New.jsx
@@ -3,7 +3,7 @@ const React = require('react')
 class New extends React.Component {
   render() {
     const date = new Date();
-    const futureDate = date.getDate() + 365;
+    const futureDate = date.getDate() + 366;
     date.setDate(futureDate);
     const defaultValue = date.toLocaleDateString('en-CA');
     return (
@@ -12,9 +12,16 @@ class New extends React.Component {
             Add New Flight
         </h1>
         <form action="/flights" method="POST">
-            Airline <input type="text" name="airline" /><br />
+            Airline <input type="text" name="airline" />
             Flight Number <input type="number" name="flightNo" min={10} max={9999} /><br />
-            Departure <input type="datetime-locale" name="departs" defaultValue={defaultValue}/><br />
+            Departure <input type="datetime-locale" name="departs" defaultValue={defaultValue}/>
+            Airport <select name="airport" defaultValue={'SAN'}>
+              <option value="SAN">SAN</option>
+              <option value="AUS">AUS</option>
+              <option value="DAL">DAL</option>
+              <option value="LAX">LAX</option>
+              <option value="SEA">SEA</option>
+            </select><br />
             <input type="submit" value="Submit New Flight" />
         </form>
         <a href="/flights">Back to flights</a>
